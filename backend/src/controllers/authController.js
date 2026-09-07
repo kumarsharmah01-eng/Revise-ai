cefa1218963c951f44591c859f3f9fc05097d90b
Harsh Kumar Sharma <kumarsharmah01@gmail.com>
2026-09-07 17:27:17 +0530

    added jwt

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
-import bcrypt from "bycryptjs";
+import bcrypt from "bcryptjs";
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -12,11 +13,11 @@ export const registerUser = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-    const hashedPassowrd = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       name,
       email,
-      password: hashedPaasword,
+      password: hashedPassword,
     });
     res.status(201).json({
       message: "User registered successfully",
@@ -29,6 +30,52 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     console.log(" Registration Error:", error.message);
 
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: " Please fill all the fields!",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid User or Passowrd",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid! user or Password",
+      });
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+    res.status(200).json({
+      message: "Login Successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log("Login error:", error.message);
     res.status(500).json({
       message: "Server Error",
     });
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//auth routerrs
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
